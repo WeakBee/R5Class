18402166761e20a0e9501a316d57d0bfd9fb7f6b
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-06-06 15:48:49 +0700

    push

---
+++ app.js
@@ -0,0 +1,10 @@
+gsap.from(".kotak", {
+    scrollTrigger: {
+        trigger: "#Jadwal-Pelajaran",
+        start: "top 80%", 
+        end: "top 80%", 
+        toggleActions: "restart none none reverse",
+    },
+    y: -200,
+    duration:1,
+});
