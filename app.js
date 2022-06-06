9e4a13b7ee4dbaaaf7e0691f475da685277f4d17
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-06-06 15:54:57 +0700

    fix mobile gsap bug

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-gsap.from(".kotak", {
-    scrollTrigger: {
-        trigger: "#Jadwal-Pelajaran",
-        start: "top 80%", 
-        end: "top 80%", 
-        toggleActions: "restart none none reverse",
-    },
-    y: -200,
-    duration:1,
-});
+if (window.matchMedia("(max-width: 992px)").matches) {
+  } else {
+    gsap.from(".kotak", {
+        scrollTrigger: {
+            trigger: "#Jadwal-Pelajaran",
+            start: "top 80%", 
+            end: "top 80%", 
+            toggleActions: "restart none none reverse",
+        },
+        y: -200,
+        duration:1,
+    });
+  }
